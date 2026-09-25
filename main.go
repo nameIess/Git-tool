3557ec9352402415412dbf377dfc780a81d5200b
Mr.A <145839706+nameIess@users.noreply.github.com>
2026-09-26 00:41:55 +0530

    refactor(core): harden GUI backend operations

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	windows "github.com/wailsapp/wails/v2/pkg/options/windows"
 
 	"github.com/nameIess/git-tool/internal/gui"
 	"github.com/nameIess/git-tool/internal/logger"
@@ -27,7 +28,6 @@ func main() {
 	if err := os.MkdirAll(logDir, 0700); err != nil {
 		fmt.Fprintf(os.Stderr, "warning: cannot create log directory: %v\n", err)
 	}
-
 	log, err := logger.Init(logDir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "warning: cannot initialize logger: %v\n", err)
@@ -46,10 +46,7 @@ func main() {
 		BackgroundColour: &options.RGBA{R: 13, G: 17, B: 23, A: 1},
 		OnStartup: app.Startup,
 		Bind: []interface{}{app},
-		Windows: &windows.Options{
-			WebviewIsTransparent: false,
-			WindowIsTranslucent: false,
-		},
+		Windows: &windows.Options{Theme: windows.SystemDefault},
 	})
 	if err != nil {
 		logger.Error("GUI exited with error: %v", err)
